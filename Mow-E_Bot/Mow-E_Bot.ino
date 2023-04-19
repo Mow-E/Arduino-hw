053e6ce488883b9c34399ca6c55f794280a93eb8
Guzz00 <81687624+Guzz00@users.noreply.github.com>
2023-04-19 13:13:04 +0200

    Update Mow-E_Bot.ino
    Added functions that can make the LED-ring on top of MowE to either blink (3x) or stays on. send a big 'L' for static light following an R,G,B or W for color. An 'l' + Color for blinky.

--- Mow-E_Bot/Mow-E_Bot.ino
+++ Mow-E_Bot/Mow-E_Bot.ino
@@ -1,14 +1,24 @@
 
 #include "MeAuriga.h"
 
+#define red 255, 0, 0
+#define green 0, 255, 0
+#define blue 0, 0, 255
+#define white 255, 255, 255
+#define black 0, 0, 0
+
+
 MeUltrasonicSensor ultraSensor(PORT_10);
 MeEncoderOnBoard motor1(SLOT1);
 MeEncoderOnBoard motor2(SLOT2);
 MeLineFollower lineFollower(PORT_9);
+MeRGBLed rgbLED(0, 12);
 
 const int BUFFER_SIZE = 128;
 const int SPEED = 100;
 const int STOP = 0;
+
+
 
 char incomingMessage[BUFFER_SIZE] = { 0 };
 
@@ -19,11 +29,14 @@ bool isStop = true;
 
 void setup() {
   Serial.begin(9600);
+  rgbLED.setpin(44);
+  //rgbLED.setColorAt(11, 0,0,0);
 }
 
 void loop() {
   handleSerialCommunication();
   motorController();
+  setLEDLoop(0, 0, 0);
 }
 
 void handleSerialCommunication() {
@@ -54,8 +67,32 @@ void handleReceivedMessage() {
   if (incomingMessage[0] == 'U') {
     Serial.print(getUltrasonicDistance());
   }
-  if (incomingMessage[0] == 'L'){
+  if (incomingMessage[0] == 'E') {
     Serial.print(getlightSensor());
+  }
+  if (incomingMessage[0] == 'L' && incomingMessage[1] == 'R') {
+    setLEDLoop(red);
+  }
+  if (incomingMessage[0] == 'L' && incomingMessage[1] == 'G') {
+    setLEDLoop(green);
+  }
+  if (incomingMessage[0] == 'L' && incomingMessage[1] == 'B') {
+    setLEDLoop(blue);
+  }
+  if (incomingMessage[0] == 'L' && incomingMessage[1] == 'W') {
+    setLEDLoop(white);
+  }
+  if (incomingMessage[0] == 'l' && incomingMessage[1] == 'R') {
+    setLEDLoopBlink(red);
+  }
+  if (incomingMessage[0] == 'l' && incomingMessage[1] == 'G') {
+    setLEDLoopBlink(green);
+  }
+  if (incomingMessage[0] == 'l' && incomingMessage[1] == 'B') {
+    setLEDLoopBlink(blue);
+  }
+  if (incomingMessage[0] == 'l' && incomingMessage[1] == 'W') {
+    setLEDLoopBlink(white);
   }
 }
 
@@ -75,10 +112,26 @@ void motorController() {
   if (isRotate) {
   }
 }
-int getlightSensor(){
+int getlightSensor() {
   return lineFollower.readSensors();
 }
 
 double getUltrasonicDistance() {
   return ultraSensor.distanceCm();
+}
+
+void setLEDLoop(uint8_t r, uint8_t g, uint8_t b) {
+  for (int i = 0; i <= 12; i++) {
+    rgbLED.setColorAt(i, r, g, b);
+    rgbLED.show();
+  }
+}
+
+void setLEDLoopBlink(uint8_t r, uint8_t g, uint8_t b) {
+  for (int i = 0; i < 3; i++) {
+    setLEDLoop(r, g, b);
+    delay(1000);
+    setLEDLoop(black);
+    delay(1000);
+  }
 }
