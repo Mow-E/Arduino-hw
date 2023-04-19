187c450b56559c072ca304fed8f9f0b8cd15879f
Guzz00 <81687624+Guzz00@users.noreply.github.com>
2023-04-19 10:42:47 +0200

    Update Mow-E_Bot.ino
    A cleaner and more optimized code  that handles the serial communication.

--- Mow-E_Bot/Mow-E_Bot.ino
+++ Mow-E_Bot/Mow-E_Bot.ino
@@ -9,10 +9,8 @@ const int BUFFER_SIZE = 128;
 const int SPEED = 100;
 const int STOP = 0;
 
-char incomingMessage[BUFFER_SIZE];
-char msg;
+char incomingMessage[BUFFER_SIZE] = { 0 };
 
-bool messageReceived = false;
 bool isForward = false;
 bool isBackward = false;
 bool isRotate = false;
@@ -20,83 +18,60 @@ bool isStop = true;
 
 void setup() {
   Serial.begin(9600);
-  memset(incomingMessage, 0 , BUFFER_SIZE);
 }
 
 void loop() {
-    handleSerialCommunication();
-    motorController();
+  handleSerialCommunication();
+  motorController();
 }
 
-void handleSerialCommunication(){
-  static int bufferIndex = 0;
-  while(Serial.available() > 0){
-    char incomingChar = (char)Serial.read();
-    if(incomingChar == '\n' || bufferIndex >= BUFFER_SIZE -1){
-      incomingMessage[bufferIndex] = '\0';
-      messageReceived = true;
-      bufferIndex = 0;
-      sendConfirmationMessage();
-      break;
-    }
-    else{
-      incomingMessage[bufferIndex++] = incomingChar;
-    }
+void handleSerialCommunication() {
+  while (Serial.available() > 0) {
+    Serial.readBytesUntil('\n', incomingMessage, BUFFER_SIZE);
+    handleReceivedMessage();
   }
-  if (messageReceived) {
-      handleReceivedMessage();
-      messageReceived = false;  }
 }
-
-void sendConfirmationMessage(){
-  Serial.print(incomingMessage);
-}
-
-void handleReceivedMessage(){
-  if(String(incomingMessage) == "F"){
-    Serial.print("Motor ON");
+void handleReceivedMessage() {
+  if (incomingMessage[0] == 'F') {
     isForward = true;
     isBackward = false;
     isRotate = false;
     isStop = false;
   }
-  if(String(incomingMessage) == "S"){
-    Serial.print("Motor OFF");
+  if (incomingMessage[0] == 'S') {
     isForward = false;
     isBackward = false;
     isRotate = false;
     isStop = true;
   }
-  if(String(incomingMessage) == "B"){
+  if (incomingMessage[0] == 'B') {
     isForward = false;
     isBackward = true;
     isRotate = false;
     isStop = true;
   }
-  if(String(incomingMessage) == "U"){
+  if (incomingMessage[0] == 'U') {
     Serial.print(getUltrasonicDistance());
   }
 }
 
-void motorController(){
-  if(isForward){
+void motorController() {
+  if (isForward) {
     motor1.setMotorPwm(SPEED);
     motor2.setMotorPwm(-SPEED);
   }
-  if(isBackward){
+  if (isBackward) {
     motor1.setMotorPwm(-SPEED);
     motor2.setMotorPwm(SPEED);
   }
-  if(isStop){
+  if (isStop) {
     motor1.setMotorPwm(STOP);
     motor2.setMotorPwm(STOP);
   }
-  if(isRotate){
-
+  if (isRotate) {
   }
 }
 
 double getUltrasonicDistance() {
   return ultraSensor.distanceCm();
 }
-
