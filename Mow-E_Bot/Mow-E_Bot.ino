a4703a4bc757a59c622eefb475fbde8aab25eacc
Guzz00 <81687624+Guzz00@users.noreply.github.com>
2023-04-28 11:27:59 +0200

    Implemented movement correctly
    The mower can now move in any direction, and I have started on an Auto-mode

--- Mow-E_Bot/Mow-E_Bot.ino
+++ Mow-E_Bot/Mow-E_Bot.ino
@@ -5,13 +5,17 @@
 #define BACK 1
 #define LEFT 3
 #define RIGHT 4
-#define ROTATIONFACTOR 1
+#define STOP 5
 
-#define red 255, 0, 0
-#define green 0, 255, 0
-#define blue 0, 0, 255
-#define white 255, 255, 255
-#define black 0, 0, 0
+#define MAX_DISTANCE 5
+
+#define RED 255, 0, 0
+#define GREEN 0, 255, 0
+#define BLUE 0, 0, 255
+#define WHITE 255, 255, 255
+#define BLACK 0, 0, 0
+
+const int TIME = 10;
 
 
 MeUltrasonicSensor ultraSensor(PORT_10);
@@ -21,8 +25,7 @@ MeLineFollower lineFollower(PORT_9);
 MeRGBLed rgbLED(0, 12);
 
 const int BUFFER_SIZE = 128;
-const int SPEED = 100;
-const int STOP = 0;
+const int SPEED = 80;
 
 int rotation = 0;
 
@@ -32,6 +35,7 @@ bool isForward = false;
 bool isBackward = false;
 bool isRotate = false;
 bool isStop = true;
+bool isAuto = false;
 
 void setup() {
   Serial.begin(9600);
@@ -48,7 +52,6 @@ void setup() {
 void loop() {
   handleSerialCommunication();
   motorController();
-  //move(4,100);
 }
 
 void handleSerialCommunication() {
@@ -59,23 +62,34 @@ void handleSerialCommunication() {
 }
 
 void handleReceivedMessage() {
+  if (incomingMessage[0] == 'A') {
+    Serial.println("AUTOMODE");
+    isForward = false;
+    isBackward = false;
+    isRotate = false;
+    isStop = false;
+    isAuto = true;
+  }
   if (incomingMessage[0] == 'F') {
     isForward = true;
     isBackward = false;
     isRotate = false;
     isStop = false;
+    isAuto = false;
   }
   if (incomingMessage[0] == 'S') {
     isForward = false;
     isBackward = false;
     isRotate = false;
     isStop = true;
+    isAuto = false;
   }
   if (incomingMessage[0] == 'B') {
     isForward = false;
     isBackward = true;
     isRotate = false;
-    isStop = true;
+    isStop = false;
+    isAuto = false;
   }
   if (incomingMessage[0] == 'R') {
     int tempHundred = incomingMessage[1] - '0';
@@ -87,8 +101,7 @@ void handleReceivedMessage() {
     isBackward = false;
     isRotate = true;
     isStop = false;
-
-    Serial.print(rotation);
+    isAuto = false;
   }
   if (incomingMessage[0] == 'r') {
     int tempHundred = incomingMessage[1] - '0';
@@ -100,8 +113,7 @@ void handleReceivedMessage() {
     isBackward = false;
     isRotate = true;
     isStop = false;
-
-    Serial.print(rotation);
+    isAuto = false;
   }
   if (incomingMessage[0] == 'U') {
     Serial.print(getUltrasonicDistance());
@@ -110,28 +122,28 @@ void handleReceivedMessage() {
     Serial.print(getLightSensor());
   }
   if (incomingMessage[0] == 'L' && incomingMessage[1] == 'R') {
-    setLEDLoop(red);
+    setLEDLoop(RED);
   }
   if (incomingMessage[0] == 'L' && incomingMessage[1] == 'G') {
-    setLEDLoop(green);
+    setLEDLoop(GREEN);
   }
   if (incomingMessage[0] == 'L' && incomingMessage[1] == 'B') {
-    setLEDLoop(blue);
+    setLEDLoop(BLUE);
   }
   if (incomingMessage[0] == 'L' && incomingMessage[1] == 'W') {
-    setLEDLoop(white);
+    setLEDLoop(WHITE);
   }
   if (incomingMessage[0] == 'l' && incomingMessage[1] == 'R') {
-    setLEDLoopBlink(red);
+    setLEDLoopBlink(RED);
   }
   if (incomingMessage[0] == 'l' && incomingMessage[1] == 'G') {
-    setLEDLoopBlink(green);
+    setLEDLoopBlink(GREEN);
   }
   if (incomingMessage[0] == 'l' && incomingMessage[1] == 'B') {
-    setLEDLoopBlink(blue);
+    setLEDLoopBlink(BLUE);
   }
   if (incomingMessage[0] == 'l' && incomingMessage[1] == 'W') {
-    setLEDLoopBlink(white);
+    setLEDLoopBlink(WHITE);
   }
 }
 void motorController() {
@@ -142,26 +154,45 @@ void motorController() {
     move(BACK, SPEED);
   }
   if (isStop) {
-    move(FORWARD, STOP);
+    move(STOP, 0);
   }
   if (isRotate) {
     if (rotation > 0) {
       move(LEFT, SPEED);
-      // _delay(rotation * ROTATIONFACTOR);
-      // move(LEFT, STOP);
-      // isRotate = false;
     }
     if (rotation < 0) {
       move(RIGHT, SPEED);
-      // _delay(rotation * ROTATIONFACTOR);
-      // move(RIGHT, STOP);
-      // isRotate = false;
     }
+  }
+  if (isAuto) {
+    autonomous();
   }
 }
 
-double getUltrasonicDistance() {
-  return ultraSensor.distanceCm();
+void move(int direction, int speed) {
+  int leftSpeed = 0;
+  int rightSpeed = 0;
+  if (direction == FORWARD) {
+    leftSpeed = speed;
+    rightSpeed = -speed;
+  } else if (direction == BACK) {
+    leftSpeed = -speed;
+    rightSpeed = speed;
+  } else if (direction == LEFT) {
+    leftSpeed = -speed;
+    rightSpeed = -speed;
+  } else if (direction == RIGHT) {
+    leftSpeed = speed;
+    rightSpeed = speed;
+  } else if (direction == STOP) {
+    leftSpeed = 0;
+    rightSpeed = 0;
+  }
+  Encoder_1.setTarPWM(leftSpeed);
+  Encoder_2.setTarPWM(rightSpeed);
+
+  Encoder_1.loop();
+  Encoder_2.loop();
 }
 
 void isr_process_encoder1(void) {
@@ -178,28 +209,7 @@ void isr_process_encoder2(void) {
     Encoder_2.pulsePosPlus();
   }
 }
-void move(int direction, int speed) {
-  int leftSpeed = 0;
-  int rightSpeed = 0;
-  if (direction == BACK) {
-    leftSpeed = -speed;
-    rightSpeed = speed;
-  } else if (direction == FORWARD) {
-    leftSpeed = speed;
-    rightSpeed = -speed;
-  } else if (direction == LEFT) {
-    leftSpeed = -speed;
-    rightSpeed = -speed;
-  } else if (direction == RIGHT) {
-    leftSpeed = speed;
-    rightSpeed = speed;
-  }
-  Encoder_1.setTarPWM(leftSpeed);
-  Encoder_2.setTarPWM(rightSpeed);
 
-  Encoder_1.loop();
-  Encoder_2.loop();
-}
 void _delay(float seconds) {
   if (seconds < 0.0) {
     seconds = 0.0;
@@ -212,6 +222,11 @@ void _delay(float seconds) {
 int getLightSensor() {
   return lineFollower.readSensors();
 }
+
+double getUltrasonicDistance() {
+  return ultraSensor.distanceCm();
+}
+
 void setLEDLoop(uint8_t r, uint8_t g, uint8_t b) {
   for (int i = 0; i <= 12; i++) {
     rgbLED.setColorAt(i, r, g, b);
@@ -222,7 +237,51 @@ void setLEDLoopBlink(uint8_t r, uint8_t g, uint8_t b) {
   for (int i = 0; i < 3; i++) {
     setLEDLoop(r, g, b);
     delay(1000);
-    setLEDLoop(black);
+    setLEDLoop(BLACK);
     delay(1000);
   }
+}
+
+void autonomous() {
+  if (getUltrasonicDistance() > MAX_DISTANCE && getLightSensor() == 3) {
+    setLEDLoop(GREEN);
+    // Serial.println("mowing...");
+    move(FORWARD, SPEED);
+  } else {
+    //Serial.println("OUTSIDE");
+    setLEDLoop(RED);
+    moveUntil(STOP);
+    if (getUltrasonicDistance() <= MAX_DISTANCE) {
+      Serial.println("TOO CLOSE");
+      //takePicture();
+      moveUntil(BACK);
+      moveUntil(RIGHT);
+    }
+    if (getLightSensor() != 3) {
+      if (getLightSensor() == 1) {
+        Serial.println("LEFT EYE CLOSED");
+        moveUntil(BACK);
+        moveUntil(LEFT);
+      }
+      if (getLightSensor() == 2) {
+        Serial.println("RIGHT EYE CLOSED");
+        moveUntil(BACK);
+        moveUntil(RIGHT);
+      } else {
+        Serial.println("BOTH EYES CLOSED");
+        moveUntil(BACK);
+        moveUntil(RIGHT);
+      }
+    }
+  }
+}
+void takePicture() {
+  Serial.print("C");
+}
+
+void moveUntil(int DIRECTION) {
+  for (int s = 0; s < TIME; s + 1) {
+    move(DIRECTION, SPEED);
+  }
+  Serial.print("Done MOVING");
 }
